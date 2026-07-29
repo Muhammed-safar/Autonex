47a54322cd809ec04d9673f40ef6acb66b01b7a5
muhammedsinank881-code <muhammedsinank881@gmail.com>
2026-07-29 15:28:06 +0530

    complete product

--- frontend/src/api/dashboard.api.js
+++ frontend/src/api/dashboard.api.js
@@ -1,6 +1,6 @@
 import API from "./axios";
 
 export const getDashboardStats = async () => {
-  const { data } = await axios.get("/brands");
+  const { data } = await API.get("/dashboard");
   return data;
 };
