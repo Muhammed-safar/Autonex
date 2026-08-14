6727c132c65ed5d0b0600e6e3f90ec4443fa32a3
Muhammed Safar <your-email@gmail.com>
2026-08-14 09:18:23 +0530

    order list admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ import AdminLayout from "./components/admin/AdminLayout";
 import Categories from "./components/admin/category/Categories.jsx";
 import Products from "./components/admin/products/Products.jsx"
 import Brands from "./components/admin/brand/Brands.jsx";
-import Orders from "./components/admin/Orders";
+import Orders from "./components/admin/orders/Orders.jsx";
 import UsersView from "./components/admin/users/UsersView.jsx";
 import VerifyOTPPage from "./pages/Login/VerifyOTPPage";
 import ResetPassword from "./pages/Login/ResetPassword";
