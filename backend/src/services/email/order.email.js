70ab17778a613a8b1eaee786b4343d2807ca7c4c
Muhammed Safar <your-email@gmail.com>
2026-08-19 09:35:41 +0530

    order email error

--- backend/src/services/email/order.email.js
+++ backend/src/services/email/order.email.js
@@ -1,6 +1,28 @@
 import { sendEmail } from "./email.service.js";
 import { baseTemplate } from "./baseTemplate.js";
 
+const orderTemplate = (order, message) => {
+    return `
+        <p>${message}</p>
+
+        <h3>Order Details</h3>
+
+        <p>
+            <strong>Order Number:</strong>
+            ${order.orderNumber}
+        </p>
+
+        <p>
+            <strong>Status:</strong>
+            ${order.orderStatus}
+        </p>
+
+        <p>
+            <strong>Total Amount:</strong>
+            ₹${order.totalAmount}
+        </p>
+    `;
+};
 
 export const sendOrderStatusEmail = async ({
     order,
